19dc208ec8ce05b438682f965c898f392d4bd719
qqzhao <qqzhao2010@gmail.com>
2016-01-26 17:58:24 +0800

    Update MAXAutoDictionary.h

--- MAXAutoDictionary.h
+++ MAXAutoDictionary.h
@@ -7,10 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "UIKit/UIKit.h"
 
 @interface MAXAutoDictionary : NSObject
 
 //可以是其他类型，并不一定是NSDate
 @property (nonatomic, strong) NSDate *date;
+
+@property (nonatomic, strong) NSString *string;
+
+@property (nonatomic, strong) NSNumber *number;
+
+@property (nonatomic, strong) UIView   *view;
+
++ setClassString:(NSString *)string;
 
 @end
